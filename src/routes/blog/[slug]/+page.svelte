9d60b070c872e3e6f15c944cb776b7d69533422c
Mimori256 <mimorimori256@gmail.com>
2023-12-02 14:32:38 +0900

    add twitter share button

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import SvelteMarkdown from 'svelte-markdown';
 	import { MetaTags } from 'svelte-meta-tags';
+	import ShareButton from './component/ShareButton.svelte';
 	export let data;
+	let url: string;
+	let textContent: string;
 	const source = data.content;
+
+	onMount(() => {
+		url = `https://mimorimori.work/blog/${data.metadata.slug}`;
+		textContent = `${data.metadata.title} - Mimori's Sandbox`;
+	});
 </script>
 
 <MetaTags
@@ -24,8 +33,12 @@
 	{#if !data.metadata}
 		<h1 class="text-3xl">ページが存在しません！</h1>
 	{:else}
+		<hr />
 		<h1 class="text-3xl">{data.metadata.title}</h1>
-		<p class="text-slate-500 mb-10">{data.metadata.date}</p>
+		<p class="text-slate-500 mb-4">{data.metadata.date}</p>
+		<div class="mb-8">
+			<ShareButton {url} {textContent} />
+		</div>
 		<article class="prose max-w-none">
 			<SvelteMarkdown {source} />
 		</article>
---
+++ src/routes/blog/[slug]/component/ShareButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let url: string;
+	export let textContent: string;
+
+	const openTwitter = () => {
+		const twitterUrl = `https://twitter.com/intent/tweet?text=${textContent}&url=${url}`;
+		window.open(twitterUrl, '_blank');
+	};
+</script>
+
+<button title="X/Twitter で共有" on:click={openTwitter}>
+	<img src="/x-logo-black.png" alt="Share on X/Twitter" height="24px" width="24px" />
+</button>
